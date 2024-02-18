67ae7537ac66ae8b33d8ede342f1fa2057504728
Rassim Benmhamed <rassimbenmhamed@outlook.com>
2024-02-18 12:22:01 -0600

    Create the template as shown in the wireframe above, with 4 separate forms, added secret key to server.py

--- server.py
+++ server.py
@@ -1,10 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask
 app = Flask(__name__)
 app.secret_key = "8891techa0128"
 
-@app.route("/")
-def index():
-    return render_template("index.html")
 
 if __name__ == ("__main__"):
     app.run(debug=True)
