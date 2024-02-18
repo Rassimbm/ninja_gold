c3ab8c1f9cf905b39c54e786f624a931a9eaa2ca
Rassim Benmhamed <rassimbenmhamed@outlook.com>
2024-02-18 14:08:01 -0600

    Have the '/process-money' POST route increase/decrease the user's gold by an appropriate amount and redirect to the root route

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-from flask import Flask, session, render_template
-
+from flask import Flask, session, render_template, request, redirect
+import random
+from datetime import datetime
 app = Flask(__name__)
 app.secret_key = "8891techa0128"
 
@@ -9,8 +10,20 @@ def index():
         session["gold"] = 0
     if "activities" not in session:
         session["activities"] = []
-    return render_template("index.html", gold=session["gold"], activities=["activities"])
+    return render_template("index.html", gold=session["gold"], activities=session["activities"])
 
+@app.route("/process-money", methods=["POST"])
+def process_money():
+    locations = {
+        "farm": {"min":10, "max": 20},
+        "cave": {"min":5, "max": 10},
+        "house": {"min":2, "max": 5},
+        "casino": {"min":-50, "max": 28}
+    }
+    location = request.form["location"]
+    earning = random.randint(locations[location]["min"], locations[location]["max"])
+    session["gold"] += earning
+    return redirect("/")
 
 if __name__ == ("__main__"):
     app.run(debug=True)
