99e3bc4223f227165b5d19a6f255b5caf39ec7bd
Rassim Benmhamed <rassimbenmhamed@outlook.com>
2024-02-18 12:26:53 -0600

    Have the root route render this page

--- server.py
+++ server.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, session, render_template
+
 app = Flask(__name__)
 app.secret_key = "8891techa0128"
+
+@app.route("/")
+def index():
+    if "gold" not in session:
+        session["gold"] = 0
+    if "activities" not in session:
+        session["activities"] = []
+    return render_template("index.html", gold=session["gold"], activities=["activities"])
 
 
 if __name__ == ("__main__"):
